38b56fffbb275c65bbe218c3fc956a263dd9a512
wildcat510 <bloomjazzer@gmail.com>
2018-03-12 22:52:23 -0700

    golden ratio transforms

---
+++ assets/js/triangle.js
@@ -0,0 +1,9 @@
+let PHI = (1 + Math.sqrt(5)) / 2;
+// for (let pct = 0; pct < 195; pct += .5) {
+//     let hr = `<div class='line' style='width:${pct}%; transform: rotate(${pct*PHI}deg); border-bottom: 1.25px solid rgba(0, 0, 0, .1); '>`;
+//     document.getElementById("net").innerHTML += hr;
+// }
+for (let pct1 = 0; pct1 < 195; pct1 += .5) {
+    let hr1 = `<div class='line' style='width:${pct1}%; transform: rotate(${pct1*PHI}deg); border-bottom: 1.25px solid rgba(0, 0, 0, .1); '>`;
+    document.getElementById("net2").innerHTML += hr1;
+}

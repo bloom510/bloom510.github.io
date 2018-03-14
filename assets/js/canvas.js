8be5c78d2952be7541e4ed0d50a2bf4304a5ee6a
wildcat510 <bloomjazzer@gmail.com>
2018-03-13 21:26:45 -0700

    mandala on document ready

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -15,51 +15,53 @@ var x = width / 2, //Initial x value at center stage
 
 // let PHI = (1 + Math.sqrt(5)) / 2;
 
-//Canvas setup
-ctx.beginPath()
-ctx.lineJoin = "round";
-ctx.lineCap = "round";
-ctx.lineWidth = .0075;
+function mandala() {
+    //Canvas setup
+    ctx.beginPath()
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+    ctx.lineWidth = .0075;
 
-//Optional colors for text if you like
-var colors = ['#ff77aa', '#aaff77', '#77aaff', '#ffffff', '#000000'];
+    //Optional colors for text if you like
+    var colors = ['#ff77aa', '#aaff77', '#77aaff', '#ffffff', '#000000'];
 
-//The meat and potatoes
-function goldenRotation(num) {
+    //The meat and potatoes
+    function goldenRotation(num) {
 
-    ctx.moveTo(x, y); //begin drawing from the start XY values
-    ctx.lineTo(x + (num * num), y); //You can play around with this to change up the pattern 
-
-
-    x -= stepX; //increment x
-    y += stepY //increment y
-
-    ctx.translate((canvas.width), (canvas.height) / 2); //rotate around a center point
-    ctx.rotate((Math.PI * PHI) * .5); //circular rotation by the golden number
-    ctx.stroke(); //render to canvas
-
-    ctx.font = '80px courier';
-    ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
-    // ctx.fillText('hello world!', width / 2, height / 2);
+        ctx.moveTo(x, y); //begin drawing from the start XY values
+        ctx.lineTo(x + (num * num), y); //You can play around with this to change up the pattern 
 
 
-    //if image has filled the frame, stop drawing and do stuff
-    if (x > width || y > height) {
-        let playlist = `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" id='playlist' src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/456397005&color=%237c948c&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=false"></iframe>`
-        clearInterval(myTimer);
-        ctx.resetTransform();
-        $('#canvas').fadeOut(3000)
-        $('#container').append(playlist)
-        $('#container').fadeIn(6000);
-        $('#triangle2').fadeIn(6000);
+        x -= stepX; //increment x
+        y += stepY //increment y
+
+        ctx.translate((canvas.width), (canvas.height) / 2); //rotate around a center point
+        ctx.rotate((Math.PI * PHI) * .5); //circular rotation by the golden number
+        ctx.stroke(); //render to canvas
+
+        ctx.font = '80px courier';
+        ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+        // ctx.fillText('hello world!', width / 2, height / 2);
+
+
+        //if image has filled the frame, stop drawing and do stuff
+        if (x > width || y > height) {
+            let playlist = `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" id='playlist' src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/456397005&color=%237c948c&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=false"></iframe>`
+            clearInterval(myTimer);
+            ctx.resetTransform();
+            $('#canvas').fadeOut(3000)
+            $('#container').append(playlist)
+            $('#container').fadeIn(6000);
+            $('#triangle2').fadeIn(6000);
+
+        }
+
+
+
 
     }
-
-
-
-
+    //Could make this recursive instead of iterated by a timer.
+    var myTimer = setInterval(function() {
+        goldenRotation(100)
+    }, 5);
 }
-//Could make this recursive instead of iterated by a timer.
-var myTimer = setInterval(function() {
-    goldenRotation(100)
-}, 5);
